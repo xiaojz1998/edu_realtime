7dfd1d0357244d53a2342cebb1db0e57f7e2a0e1
xiaojz <xiaojz1998@outlook.com>
2024-12-13 21:16:24 +0800

    init test project

---
+++ realtime-common/src/main/java/com/atguigu/edu/realtime/common/base/baseApp.java
@@ -0,0 +1,12 @@
+package com.atguigu.edu.realtime.common.base;
+
+/**
+ * Title: baseApp
+ * Create on: 2024/12/13 21:15
+ *
+ * @author Xiao Jianzhe
+ * @version 1.0.0
+ * Description:
+ */
+public class baseApp {
+}
---
+++ realtime-common/src/main/java/com/atguigu/edu/realtime/common/util/test.java
@@ -0,0 +1,12 @@
+package com.atguigu.edu.realtime.common.util;
+
+/**
+ * Title: test
+ * Create on: 2024/12/13 21:16
+ *
+ * @author Xiao Jianzhe
+ * @version 1.0.0
+ * Description:
+ */
+public class test {
+}
