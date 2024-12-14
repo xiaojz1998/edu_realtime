f5813aae9f776e8c727b1ffc4877ad71f2d08b40
xiaojz <xiaojz1998@outlook.com>
2024-12-15 00:44:30 +0800

    2024/12/15 dim抽取

--- realtime-dim/src/main/java/com/atguigu/edu/realtime/dim/app/DimApp.java
+++ realtime-dim/src/main/java/com/atguigu/edu/realtime/dim/app/DimApp.java
@@ -2,6 +2,7 @@ package com.atguigu.edu.realtime.dim.app;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.edu.realtime.common.base.baseApp;
 import com.atguigu.edu.realtime.common.bean.TableProcess;
 import com.atguigu.edu.realtime.common.constant.Constant;
 import com.atguigu.edu.realtime.common.util.FlinkSourceUtil;
@@ -45,45 +46,20 @@ import java.util.Set;
  *  需要启动的进程:
  *    zk、kafka、maxwell、hdfs、hbase、DimApp
  */
-public class DimApp {
+public class DimApp extends baseApp {
     public static void main(String[] args) {
-        //TODO 1.基本环境准备
-        //1.1 指定流处理环境
-        Configuration conf = new Configuration();
-        conf.set(RestOptions.PORT,8088);
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
-        //1.2 设置并行度
-        env.setParallelism(4);
 
-        //TODO 2.检查点相关的设置
-        //2.1 开启检查点
-        env.enableCheckpointing(5000L, CheckpointingMode.EXACTLY_ONCE);
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,3000L));
-        /*
+        new DimApp().start(
+                10002,
+                4,
+                "dim_app",
+                Constant.TOPIC_DB
+        );
 
-        CheckpointConfig checkpointConfig = env.getCheckpointConfig();
-        //2.2 设置检查点超时时间
-        checkpointConfig.setCheckpointTimeout(60000L);
-        //2.3 设置job取消之后检查点是否保留
-        checkpointConfig.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-        //2.4 设置两个检查点之间最小时间间隔
-        checkpointConfig.setMinPauseBetweenCheckpoints(2000L);
-        //2.5 设置重启策略
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,3000L));
-        //2.7 设置检查点存储路径
-        checkpointConfig.setCheckpointStorage("hdfs://hadoop102:8020/ck");
-        //2.8 设置操作hadoop的用户
-        System.setProperty("HADOOP_USER_NAME","atguigu");
+    }
 
-         */
-        //TODO 3.从kafka主题中读取主流业务数据
-        //3.2 创建消费者对象
-        KafkaSource<String> kafkaSource = FlinkSourceUtil.getKafkaSource(Constant.TOPIC_DB, "dim_app");
-        //3.3 消费数据 封装为流
-        DataStreamSource<String> kafkaSourceDS = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source");
-
-        //kafkaSourceDS.print();
-
+    @Override
+    public void handle(StreamExecutionEnvironment env, DataStreamSource<String> kafkaSourceDS) {
         // kafka 数据格式
         //{"database":"edu","table":"test_exam_question","type":"insert","ts":1734144588,
         // "xid":187609,"commit":true,"data":{"id":63576,"exam_id":4310,"paper_id":204,
@@ -203,10 +179,5 @@ public class DimApp {
         dimDS.addSink(
                 new DimSinkFunction()
         );
-        try {
-            env.execute();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
