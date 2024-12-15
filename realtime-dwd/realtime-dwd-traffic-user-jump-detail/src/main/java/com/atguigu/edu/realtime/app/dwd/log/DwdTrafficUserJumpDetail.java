e472661360bc0df10154b2b729314a977690be3e
zhengranran <2998643453@qq.com>
2024-12-15 22:11:40 +0800

    流量域用户跳出事务事实表 过滤用户跳出明细数据。

--- realtime-dwd/realtime-dwd-traffic-user-jump-detail/src/main/java/com/atguigu/edu/realtime/app/dwd/log/DwdTrafficUserJumpDetail.java
+++ realtime-dwd/realtime-dwd-traffic-user-jump-detail/src/main/java/com/atguigu/edu/realtime/app/dwd/log/DwdTrafficUserJumpDetail.java
@@ -3,6 +3,7 @@ package com.atguigu.edu.realtime.app.dwd.log;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.edu.realtime.common.base.baseApp;
 import com.atguigu.edu.realtime.common.constant.Constant;
+import com.atguigu.edu.realtime.common.util.FlinkSinkUtil;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -62,19 +63,67 @@ public class DwdTrafficUserJumpDetail extends baseApp {
                                     }
                                 }
                         ));
-
+        //WatermarkDS.print();
         //TODO 3.按照Mid分组
         KeyedStream<JSONObject, String> keyByjsonObjDS
                 = WatermarkDS.keyBy(jsonObject -> jsonObject.getJSONObject("common").getString("mid"));
+        //keyByjsonObjDS.print();
         //TODO 4 定义cep匹配规则
-
+        Pattern<JSONObject, JSONObject> pattern = Pattern.<JSONObject>begin("first").where(
+                new IterativeCondition<JSONObject>() {
+                    //一个会话的开头，last_page_id为空
+                    @Override
+                    public boolean filter(JSONObject jsonObject, Context<JSONObject> context) throws Exception {
+                        return jsonObject.getJSONObject("common").getString("last_page_id") == null;
+                    }
+                }
+        ).next("second").where(
+                new IterativeCondition<JSONObject>() {
+                    @Override
+                    public boolean filter(JSONObject jsonObject, Context<JSONObject> context) throws Exception {
+                        return jsonObject.getJSONObject("common").getString("last_page_id") == null;
+                    }
+                }
+        ).within(Time.seconds(10L)); //指定整个模式必须在 10 秒内完成匹配，否则视为超时
         //TODO 5 将CEP作用到流上
+        PatternStream<JSONObject> patternjsonObjDS = CEP.pattern(keyByjsonObjDS, pattern);
         //TODO 6 提取匹配数据和超时数据
+           //定义一个侧输出流用来接收超时数据
+        OutputTag<String> timeOutTag = new OutputTag<String>("timeOutTag"){};
+        SingleOutputStreamOperator<String> flatSelectDS = patternjsonObjDS.flatSelect(timeOutTag,
+                new PatternFlatTimeoutFunction<JSONObject, String>() {
+
+                    @Override
+                    public void timeout(Map<String, List<JSONObject>> pattern, long timeoutTimestamp, Collector<String> out) throws Exception {
+                        //这个方法用来处理超时数据
+                        //如果map.get("first")返回一个非空集合，则取map第一个元素,并赋值给first，并输出给下游处理逻辑
+                        if (pattern.containsKey("first")) {
+                            List<JSONObject> firstEvents = pattern.get("first");
+                            if (!firstEvents.isEmpty()) {
+                                JSONObject first = firstEvents.get(0);
+                                out.collect(first.toJSONString());
+                            } else {
+                                // 处理列表为空的情况
+                                System.out.println("The 'first' list is empty at timeout.");
+                            }
+                        } else {
+                            // 处理键 "first" 不存在的情况
+                            System.out.println("Key 'first' does not exist in the pattern at timeout.");
+                        }
+                    }
+                }, new PatternFlatSelectFunction<JSONObject, String>() {
+                    @Override
+                    public void flatSelect(Map<String, List<JSONObject>> pattern, Collector<String> out) throws Exception {
+                        //这个方法用来处理匹配数据
+                        JSONObject first = pattern.get("first").get(0);
+                        out.collect(first.toJSONString());
+                    }
+                });
+        //flatSelectDS.print();
+        SideOutputDataStream<String> timeoutDS = flatSelectDS.getSideOutput(timeOutTag);
         //TODO 7 合并数据写出到kafka
-
-
-
-
-
+        DataStream<String> unionDS = flatSelectDS.union(timeoutDS);
+        //unionDS.print();
+        unionDS.sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_USER_JUMP_DETAIL));
     }
 }
